b0d5e566de2a6ea0b1ff452eec8430917ca78a0c
rohitgeddam <rohitgeddam2018@gmail.com>
2022-06-22 23:17:54 +0530

    changed api url

--- constants.js
+++ constants.js
@@ -2,8 +2,7 @@ let PRIMARY_BTN_COLOR = "#0BCE83";
 let SECONDARY_BTN_COLOR = "#94F3E3";
 let PRIMARY_BACKGROUND_COLOR = "#4BB09F"
 let PRIMARY_BACKGROUND_DARK_COLOR = "#429690";
-// let API_URL = "https://notifier-bitd.herokuapp.com"
-let API_URL = "https://3c3d-2405-201-3005-e085-22ec-a3f6-cf58-b989.ngrok.io"
+let API_URL = "https://notifier-bitd.herokuapp.com"
 
 
 export { PRIMARY_BTN_COLOR, PRIMARY_BACKGROUND_COLOR,PRIMARY_BACKGROUND_DARK_COLOR, SECONDARY_BTN_COLOR, API_URL }
